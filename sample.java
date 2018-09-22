2413947c036eb04aa57ca806448f8fd865de10b2
youichimikata <gibson_guitar_classic@hotmail.com>
2018-09-22 14:17:22 +0900

    sample

---
+++ sample.java
@@ -0,0 +1,15 @@
+public class Sample {
+
+  public static void main(String[] args) {
+    //‘½ŸŒ³”z—ñ‚ÌéŒ¾
+    String[][] testScore = {
+      { "‘Œê", "80" }
+      , { "Z”", "90" }
+      , { "Ğ‰ï", "70" }
+      , { "—‰È", "75" }
+    };
+
+    System.out.println("‹³‰È:" + testScore[1][0] + "A“_”:" + testScore[1][1]);
+  }
+
+}
